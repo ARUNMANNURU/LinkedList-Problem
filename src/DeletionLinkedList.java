c0537ed130aba7876296b0e1a9e2f7aa9ddc26a0
Mannuru <arunm057@gmail.com>
2017-07-20 16:15:51 -0400

    psitionbased deletion

--- src/DeletionLinkedList.java
+++ src/DeletionLinkedList.java
@@ -73,6 +73,40 @@ public class DeletionLinkedList {
         }
     }
 	
+    /* Given a reference (pointer to pointer) to the head of a list
+    and a position, deletes the node at the given position */
+ void deleteNodeAtPosition(int position)
+ {
+     // If linked list is empty
+     if (head == null)
+         return;
+
+     // Store head node
+     Node temp = head;
+
+     // If head needs to be removed
+     if (position == 0)
+     {
+         head = temp.next;   // Change head
+         return;
+     }
+
+     // Find previous node of the node to be deleted
+     for (int i=0; temp!=null && i<position-1; i++)
+         temp = temp.next;
+
+     // If position is more than number of ndoes
+     if (temp == null || temp.next == null)
+         return;
+
+     // Node temp->next is the node to be deleted
+     // Store pointer to the next of node to be deleted
+     Node next = temp.next.next;
+
+     temp.next = next;  // Unlink the deleted node from list
+ }
+    
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -88,6 +122,8 @@ public class DeletionLinkedList {
  
         llist.deleteNode(1); // Delete node at position 4
  
+        llist.deleteNodeAtPosition(0);
+        
         System.out.println("\nLinked List after Deletion at position 4:");
         llist.printList();
 		
